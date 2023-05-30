974119dd95905b03ce8f9764be792d8a2419c015
devanshuc <66375026+devanshuc@users.noreply.github.com>
2023-05-30 15:30:20 +0530

    Added Validation

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -6,11 +6,19 @@ import styles from "./AddUser.module.css";
 const AddUser = (props) => {
 	const addUserHandler = (event) => {
 		event.preventDefault();
-		console.log(enteredUsername, eneteredAge);
+		if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+			return;
+		}
+		if (+enteredAge < 0) {
+			return;
+		}
+		console.log(enteredUsername, enteredAge);
+		setEnteredAge("");
+		setEnteredUsername("");
 	};
 
 	const [enteredUsername, setEnteredUsername] = useState("");
-	const [eneteredAge, setEnteredAge] = useState("");
+	const [enteredAge, setEnteredAge] = useState("");
 
 	const usernameHandler = (event) => {
 		setEnteredUsername(event.target.value);
@@ -24,9 +32,19 @@ const AddUser = (props) => {
 		<Card className={styles.input}>
 			<form onSubmit={addUserHandler}>
 				<label htmlFor="username">Username</label>
-				<input type="text" id="username" onChange={usernameHandler} />
+				<input
+					type="text"
+					id="username"
+					onChange={usernameHandler}
+					value={enteredUsername}
+				/>
 				<label htmlFor="age">Age(in Years)</label>
-				<input type="number" id="age" onChange={ageHandler} />
+				<input
+					type="number"
+					id="age"
+					onChange={ageHandler}
+					value={enteredAge}
+				/>
 				<Button />
 			</form>
 		</Card>
